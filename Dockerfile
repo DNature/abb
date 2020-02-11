226012fae37b49b2603a74e2b3f953d3eb537002
DNature <divinehycenth@outlook.com>
2020-02-11 10:10:00 +0100

    edited ormconfig and createTypeormConn for production build

--- Dockerfile
+++ Dockerfile
@@ -9,10 +9,9 @@ COPY ./packages/common/package.json ./packages/common/
 RUN npm i -g yarn@1.21.1 --force
 RUN yarn install --production
 
-COPY ./packages/server/dist/ ./packages/server/
+COPY ./packages/server/dist ./packages/server/dist
 COPY ./packages/common/dist ./packages/common/dist
-
-COPY ./packages/server/.env ./packages/server/
+COPY ./packages/server/.env ./packages/server/.env
 COPY ./ormconfig.json .
 
 WORKDIR /abb/packages/server/
--- packages/server/src/utils/createTypormConn.ts
+++ packages/server/src/utils/createTypormConn.ts
@@ -1,4 +1,5 @@
 import { createConnection, getConnectionOptions } from "typeorm";
+import { User } from "../entity/User";
 
 export const createTypeormConn = async () => {
   const connectionOptions = await getConnectionOptions(process.env.NODE_ENV);
@@ -7,6 +8,7 @@ export const createTypeormConn = async () => {
     ? await createConnection({
         ...connectionOptions,
         url: process.env.DATABASE_URL,
+        entities: [User],
         name: "default"
       } as any)
     : await createConnection({ ...connectionOptions, name: "default" });
