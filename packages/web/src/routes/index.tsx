175d30621420bf3a33a3320883e95e9e61b6f02c
DNature <divinehycenth@outlook.com>
2020-02-19 15:38:00 +0100

    Added auth route

--- packages/web/src/routes/index.tsx
+++ packages/web/src/routes/index.tsx
@@ -1,20 +1,31 @@
 import * as React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { AuthRoute } from "@abb/controller";
 
 import { RegisterConnector } from "../modules/register/RegisterConnector";
 import { LoginConnector } from "../modules/login/LoginConnector";
-import { ForgotPasswordConnector } from '../modules/forgotPassword/forgotPasswordConnector';
-import { ChangePasswordConnector } from '../modules/changePassword/changePasswordConnector';
-import { TextPage } from '../modules/TextPage';
+import { ForgotPasswordConnector } from "../modules/forgotPassword/forgotPasswordConnector";
+import { ChangePasswordConnector } from "../modules/changePassword/changePasswordConnector";
+import { TextPage } from "../modules/TextPage";
+import { CreateListingConnector } from "../modules/listing/create/CreateLinstingConnector";
 
 export const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route exact={true} path="/register" component={RegisterConnector} />
       <Route exact={true} path="/login" component={LoginConnector} />
-      <Route exact={true} path="/forgot-password" component={ForgotPasswordConnector} />
-      <Route exact={true} path="/change-password/:key" component={ChangePasswordConnector} />
+      <Route
+        exact={true}
+        path="/forgot-password"
+        component={ForgotPasswordConnector}
+      />
+      <Route
+        exact={true}
+        path="/change-password/:key"
+        component={ChangePasswordConnector}
+      />
       <Route path="/m" component={TextPage} />
+      <AuthRoute path="/create-listing" component={CreateListingConnector} />
     </Switch>
   </BrowserRouter>
 );
