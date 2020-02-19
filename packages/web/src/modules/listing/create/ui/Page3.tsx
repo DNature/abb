9089a5e14a2e7734c802f605293664db81688899
DNature <divinehycenth@outlook.com>
2020-02-19 21:31:31 +0100

    fixed listing form labels

--- packages/web/src/modules/listing/create/ui/Page3.tsx
+++ packages/web/src/modules/listing/create/ui/Page3.tsx
@@ -1,24 +1,12 @@
 import * as React from "react";
 import { Field } from "formik";
+
 import { InputField } from "../../../shared/InputField";
 
 export const Page3 = () => (
-         <>
-           <Field
-             name="longitude"
-             placeholder="Longitude"
-             component={InputField}
-           />
-           <Field
-             name="latitude"
-             placeholder="Latitude"
-             component={InputField}
-           />
-
-           <Field
-             name="amenities"
-             placeholder="Amenities"
-             component={InputField}
-           />
-         </>
-       );
+  <>
+    <Field label="Price" name="longitude" placeholder="Longitude" component={InputField} />
+      <Field label="Price" name="latitude" placeholder="Latitude" component={InputField} />
+    <Field name="amenities" placeholder="Amenities" component={InputField} />
+  </>
+);
--- packages/web/src/modules/shared/InputField.tsx
+++ packages/web/src/modules/shared/InputField.tsx
@@ -3,16 +3,18 @@ import { FieldProps } from "formik";
 import { Form, Input } from "antd";
 
 export const InputField: React.SFC<FieldProps<any> & {
-  prefix: React.ReactNode;
+  prefix: React.ReactNode; label?: string
 }> = ({
   field, // { name, value, onChange, onBlur }
   form: { touched, errors }, // also values, setXXXX, handleXXXX, dirty, isValid, status, etc.
+  label,
   ...props
 }) => {
   const errorMsg = touched[field.name] && errors[field.name];
 
   return (
     <Form.Item
+    label={label}
       help={touched[field.name] && errors[field.name] ? errors[field.name] : ""}
       validateStatus={
         errorMsg ? "error" : !touched[field.name] ? "" : "success"
